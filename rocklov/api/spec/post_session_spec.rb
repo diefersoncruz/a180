244f2748c14a6e4f3e759c0b1e7bb8d0fbd2d413
Dieferson Cruz <dieferson.cruz@outlook.com>
2021-04-12 09:06:06 -0300

    Testando api

--- rocklov/api/spec/post_session_spec.rb
+++ rocklov/api/spec/post_session_spec.rb
@@ -1,18 +1,31 @@
-require "httparty"
+require_relative "routes/sessions"
 
 describe "POST /sessions" do
-  it "Login com sucesso" do
-    payload = { email: "dieff.cruz@gmail.com", password: "1234" }
+  context "Login com sucesso" do
+    before(:all) do
+      @result = Sessions.new.login("dieff.cruz@gmail.com", "1234")
+    end
 
-    result = HTTParty.post(
-      "http://rocklov-api:3333/sessions",
-      body: payload.to_json,
-      headers: {
-        "Content-Type": "application/json",
-      },
-    )
+    it "valida status code" do
+      expect(@result.code).to eql 200
+    end
 
-    expect(result.code).to eql 200
-    expect(result.parsed_response["_id"].length).to eql 24
+    it "valida id do usuário" do
+      expect(@result.parsed_response["_id"].length).to eql 24
+    end
+  end
+
+  context "Senha invalida" do
+    before(:all) do
+      @result = Sessions.new.login("dieff.cruz@gmail.com", "abcde")
+    end
+
+    it "valida status code" do
+      expect(@result.code).to eql 401
+    end
+
+    it "valida id do usuário" do
+      expect(@result.parsed_response["error"]).to eql "Unauthorized"
+    end
   end
 end
